2df1f7568e22f6257d9581b10903a9995fb3807c
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-11-25 22:09:30 -0300

    GET games by game list

--- src/main/java/com/kenmarcos/dslist/controllers/GameListController.java
+++ src/main/java/com/kenmarcos/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kenmarcos.dslist.dto.GameListDTO;
+import com.kenmarcos.dslist.dto.GameMinDTO;
 import com.kenmarcos.dslist.services.GameListService;
+import com.kenmarcos.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/gameLists") // configurar a rota
@@ -17,9 +20,19 @@ public class GameListController {
   @Autowired
   private GameListService gameListService;
 
+  @Autowired
+  private GameService gameService;
+
   @GetMapping
   public List<GameListDTO> findAll() {
     List<GameListDTO> result = gameListService.findAll();
+
+    return result;
+  }
+
+  @GetMapping(value = "/{gameListId}/games")
+  public List<GameMinDTO> findByGameList(@PathVariable Long gameListId) {
+    List<GameMinDTO> result = gameService.findByGameList(gameListId);
 
     return result;
   }
