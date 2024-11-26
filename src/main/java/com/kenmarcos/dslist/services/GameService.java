f25734ab7ac3f0eff7818c954a4c0a6c065af8e9
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-11-25 21:21:06 -0300

    GET game by id

--- src/main/java/com/kenmarcos/dslist/services/GameService.java
+++ src/main/java/com/kenmarcos/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.kenmarcos.dslist.dto.GameDTO;
 import com.kenmarcos.dslist.dto.GameMinDTO;
 import com.kenmarcos.dslist.entities.Game;
 import com.kenmarcos.dslist.repositories.GameRepository;
@@ -16,6 +18,14 @@ public class GameService {
   @Autowired
   private GameRepository gameRepository;
 
+  @Transactional(readOnly = true)
+  public GameDTO findById(Long gameId) {
+    Game result = gameRepository.findById(gameId).get();
+    GameDTO dto = new GameDTO(result);
+    return dto;
+  }
+
+  @Transactional(readOnly = true)
   public List<GameMinDTO> findAll() {
     List<Game> result = gameRepository.findAll();
     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
